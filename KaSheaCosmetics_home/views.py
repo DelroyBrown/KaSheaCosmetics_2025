07efb18a24a1ca3c4c3433d098fef753105fc8db
Delroy Brown <delroybrown.db@gmail.com>
2025-08-30 18:29:48 +0100

    Refactor home view to display featured products with images and update template to reflect changes

--- KaSheaCosmetics_home/views.py
+++ KaSheaCosmetics_home/views.py
@@ -1,5 +1,15 @@
+# views.py
 from django.shortcuts import render
-
+from django.db.models import Prefetch
+from KaSheaCosmetics_catalog.models import Product, ProductImage
 
 def home(request):
-    return render(request, "home.html")
+    img_qs = ProductImage.objects.order_by('-is_primary', 'sort_order', 'id')
+
+    featured_products = (
+        Product.objects
+        .filter(featured=True, is_active=True)
+        .prefetch_related(Prefetch('images', queryset=img_qs, to_attr='imgs'))[:4]
+    )
+
+    return render(request, "home.html", {"featured_products": featured_products})
